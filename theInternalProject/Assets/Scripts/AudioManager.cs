b62c51f772e8af19fc1f2ae855a31898a9c79e41
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-01 14:47:12 +0000

    create scripts for gameManager and AudioManager

---
+++ theInternalProject/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+// this class manages overall audio functionalities
+// background music, sound effects, etc
+public class AudioManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ theInternalProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+// this class manages the overall game state and logic
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
