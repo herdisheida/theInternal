01d67e45e46bdf6009bb0a4a30c0b308451aab60
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-01 15:31:47 +0000

    Update AudioManager.cs
    * music and SFX functionality,
    
    * helper func for clips
    
    * variables for clips

--- theInternalProject/Assets/Scripts/AudioManager.cs
+++ theInternalProject/Assets/Scripts/AudioManager.cs
@@ -4,6 +4,43 @@ using UnityEngine;
 // background music, sound effects, etc
 public class AudioManager : MonoBehaviour
 {
+    [Header("Sources")]
+    public AudioSource musicSource; // drag and drop AudioSource component in inspector (looping music)
+    public AudioSource sfxSource; // drag and drop another AudioSource component in inspector
+
+    [Header("Background Music Clips")]
+    public AudioClip hospitalLobbyMusic;
+    public AudioClip obstacleGameplayMusic;
+    public AudioClip bossBattleMusic;
+
+    [Header("Sound Effect Clips")]
+    // menu-related
+    public AudioClip buttonClickClip; // start, quit buttons
+
+    // ambient-related
+
+    // player-related
+    public AudioClip damageTakenClip;
+    public AudioClip dyingClip;
+    public AudioClip attackClip; // medicine bullet shooting
+    public AudioClip shootPatientClip; // shoot infected patient
+
+    // enemy-related
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
         
@@ -13,4 +50,38 @@ public class AudioManager : MonoBehaviour
     {
         
     }
+
+
+    public void PlayMusic()
+    {
+        if (musicSource == null || backgroundMusic == null) return;
+
+        if (musicSource.isPlaying) return; // don’t restart if already playing
+
+        musicSource.clip = backgroundMusic;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        if (musicSource != null)
+            musicSource.Stop();
+    }
+
+    void PlaySFX(AudioClip clip)
+    {
+        if (sfxSource == null || clip == null) return;
+        sfxSource.PlayOneShot(clip);
+    }
+
+
+
+    // helper methods
+    public void buttonClick()    => PlaySFX(buttonClickClip);
+    
+    public void damageTaken()    => PlaySFX(damageTakenClip);
+    public void dying()          => PlaySFX(dyingClip);
+    public void attack()         => PlaySFX(attackClip);
+    public void shootPatient()   => PlaySFX(shootPatientClip);
 }
