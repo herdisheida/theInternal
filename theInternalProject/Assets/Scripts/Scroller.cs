e5b1c0b9e7b8a69eff5cecd0608b1d0971cda5c5
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-02 10:30:30 +0000

    move obsticle to left and delete them when off screen

--- theInternalProject/Assets/Scripts/Scroller.cs
+++ theInternalProject/Assets/Scripts/Scroller.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Scroller : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;   // how fast obstacles move left
+    public float destroyX = -15f;  // pos where we delete the obstacles
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+
+        if (transform.position.x < destroyX)
+        {
+            Destroy(gameObject);
+        }
     }
 }
