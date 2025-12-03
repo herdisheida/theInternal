996b4e5931bcdc1f9f8fa7eea4e713a0265e506c
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-03 13:47:40 +0000

    crate main menu script to play the next scene

---
+++ theInternalProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("PatientSelection");
+    }
+
+}
