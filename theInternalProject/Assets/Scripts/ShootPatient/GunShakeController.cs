0af4834404342275c32d79297851d5ab0752d1cb
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-04 16:51:20 +0000

    enable shoooting space bar pic

--- theInternalProject/Assets/Scripts/ShootPatient/GunShakeController.cs
+++ theInternalProject/Assets/Scripts/ShootPatient/GunShakeController.cs
@@ -224,6 +224,7 @@ public class GunShakeController : MonoBehaviour
             return;
         }
 
+        spaceKeyImage.enabled = true;
         // use a sine wave to flip between sprites
         float v = Mathf.Sin(Time.time * spaceKeyBlinkSpeed);
 
