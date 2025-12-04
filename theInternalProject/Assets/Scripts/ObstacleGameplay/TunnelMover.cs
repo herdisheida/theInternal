d96e6cfc7a3783509cf84ee2f06ded06eb7f31a0
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-04 12:43:25 +0000

    make the background stop moving when the tunnel spanws

--- theInternalProject/Assets/Scripts/ObstacleGameplay/TunnelMover.cs
+++ theInternalProject/Assets/Scripts/ObstacleGameplay/TunnelMover.cs
@@ -5,19 +5,32 @@ public class TunnelMover : MonoBehaviour
     public float moveSpeed = 5f;
     public float stopPos = 0f; // where tunnel should stop on screen
 
+    [Header("Things to stop when tunnel arrives")]
+    public BackgroundScroller backgroundScroller;
+
+
     void Start()
     {
-        
+        if (backgroundScroller == null) { backgroundScroller = FindObjectOfType<BackgroundScroller>(); }
     }
 
     void Update()
     {
         if (transform.position.x <= stopPos)
         {
+            // snap into correct pos
             transform.position = new Vector3(stopPos, transform.position.y, transform.position.z);
-            enabled = false; // stop moving
+
+            // stop background scrolling
+            if (backgroundScroller != null)
+                backgroundScroller.StopScrolling();
+
+            // stop moving tunnel
+            enabled = false;
             return;
         }
+
+        // move tunnel left
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
     }
 }
