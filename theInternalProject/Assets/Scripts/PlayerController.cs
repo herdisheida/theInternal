78cfe76802440dbed66f70844ee33af5d7f8e7e0
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-05 22:57:48 +0000

    set left and right bounds on player

--- theInternalProject/Assets/Scripts/PlayerController.cs
+++ theInternalProject/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,9 @@ public class PlayerController : MonoBehaviour
     [Header("Movement Bounds")]
     public BoxCollider2D topBorder;
     public BoxCollider2D bottomBorder;
-    public float padding = 0.3f;           // avoid player being inside the border
+    public BoxCollider2D leftBorder;
+    public BoxCollider2D rightBorder;
+    public float padding = 0.7f; // avoid player being inside the border
 
     void Start()
     {
@@ -32,13 +34,22 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 targetPosition = rb.position + moveSpeed * moveInput * Time.fixedDeltaTime;
 
-        // fix movement within bounds
+        // top & bottom bounds
         if (topBorder != null && bottomBorder != null)
         {
             float maxY = topBorder.bounds.min.y - padding;
             float minY = bottomBorder.bounds.max.y + padding;
 
             targetPosition.y = Mathf.Clamp(targetPosition.y, minY, maxY);
+        }
+
+        // left & right bounds
+        if (leftBorder != null && rightBorder != null)
+        {
+            float minX = leftBorder.bounds.max.x + padding;
+            float maxX = rightBorder.bounds.min.x - padding;
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX);
         }
 
         rb.MovePosition(targetPosition);
