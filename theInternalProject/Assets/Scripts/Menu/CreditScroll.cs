f673785e2a5a3b6bc2433570f9a835d3b0cb5ed8
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-05 17:51:15 +0000

    credits have subtitle
    * tiny doctor big dreams
    * wait a bit before scrolling begins
    * credits scrolls after title (begins neðar)

--- theInternalProject/Assets/Scripts/Menu/CreditScroll.cs
+++ theInternalProject/Assets/Scripts/Menu/CreditScroll.cs
@@ -5,6 +5,8 @@ public class CreditScroll : MonoBehaviour
     public float scrollSpeed = 40f; // adjust the speed of the scroll
     private RectTransform rectTransform;
 
+    public float delayScrollTime = 2f; // delay before starting the scroll
+
     void Start()
     {
         // get the RectTransform component
@@ -14,6 +16,12 @@ public class CreditScroll : MonoBehaviour
 
     void Update()
     {
+        if (delayScrollTime > 0f)
+        {
+            delayScrollTime -= Time.deltaTime;
+            return;
+        }
+
         // move the credits upward
         rectTransform.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
     }
