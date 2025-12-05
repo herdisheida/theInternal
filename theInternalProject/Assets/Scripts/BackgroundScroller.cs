1687e51c10410fc4a09f91003b4d190799a3261e
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-05 15:42:50 +0000

    add is scrolling variable

--- theInternalProject/Assets/Scripts/BackgroundScroller.cs
+++ theInternalProject/Assets/Scripts/BackgroundScroller.cs
@@ -6,8 +6,12 @@ public class BackgroundScroller : MonoBehaviour
     [SerializeField] private RawImage _img;
     [SerializeField] private float _x;
 
+    private bool _isScrolling = true;
+
     void Update()
     {
+        if (!_isScrolling) return;
+
         _img.uvRect = new Rect(
             new Vector2(_img.uvRect.x + _x * Time.deltaTime, 0f),
             _img.uvRect.size
