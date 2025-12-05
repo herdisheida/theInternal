a95cc51a466fe9b8447ec9b2dbd3e2cc401c4842
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-05 15:38:27 +0000

    stop background scroller when tunnel stops

--- theInternalProject/Assets/Scripts/ObstacleGameplay/TunnelMover.cs
+++ theInternalProject/Assets/Scripts/ObstacleGameplay/TunnelMover.cs
@@ -5,6 +5,8 @@ public class TunnelMover : MonoBehaviour
     public float moveSpeed = 5f;
     public float stopPos = 0f; // where tunnel should stop on screen
 
+    public BackgroundScroller backgroundScroller; // reference to background scroller to stop it
+
     void Start()
     {
 
@@ -17,6 +19,12 @@ public class TunnelMover : MonoBehaviour
             // snap into correct pos
             transform.position = new Vector3(stopPos, transform.position.y, transform.position.z);
 
+            // stop background scrolling
+            if (backgroundScroller != null)
+            {
+                backgroundScroller.StopScrolling();
+            }
+
             // stop moving tunnel
             enabled = false;
             return;
@@ -24,5 +32,6 @@ public class TunnelMover : MonoBehaviour
 
         // move tunnel left
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+
     }
 }
