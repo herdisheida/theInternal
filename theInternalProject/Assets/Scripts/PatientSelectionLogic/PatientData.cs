18b500c265c1d05817ae5668535681e237db4ba6
Brynhildurth <brynhildur.bjorg@gmail.com>
2025-12-08 19:13:08 +0000

    Added analysis sprites to the patient data

--- theInternalProject/Assets/Scripts/PatientSelectionLogic/PatientData.cs
+++ theInternalProject/Assets/Scripts/PatientSelectionLogic/PatientData.cs
@@ -10,7 +10,9 @@ public class PatientData : ScriptableObject
     public Sprite unselected;
     public Sprite infected;
     public Sprite dead;
-    public Sprite saved;
+    public Sprite analysisInfected;
+    public Sprite analysisSafe;
+
     public bool isSaved;
 
     public PatientStatus status;
