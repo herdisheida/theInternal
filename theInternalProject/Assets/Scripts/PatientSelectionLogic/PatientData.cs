6eec728b296ad770e7f7bf560220e985d5ab8e04
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-08 12:58:02 +0000

    create manager folder
    add audio and game manager to it

--- theInternalProject/Assets/Scripts/PatientSelectionLogic/PatientData.cs
+++ theInternalProject/Assets/Scripts/PatientSelectionLogic/PatientData.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class PatientData : ScriptableObject
 {
     public string patientName;
+
     public Sprite selected;
     public Sprite unselected;
     public Sprite infected;
     public Sprite dead;
     public bool isSaved;
 
+    public PatientStatus status;
+
+}
+
+public enum PatientStatus
+{
+    None,       // not visited yet
+    Infected,   // player died before before killing boss
+    Dead,       // player shot them (after shooting patient)
+    Saved       // player beat boss
 }
