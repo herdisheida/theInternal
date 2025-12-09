862931290c47d580a9cbeae27c8a1288a083365c
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-09 10:29:25 +0000

    create ending controller
    diff text, sprite and music depending on ending

--- theInternalProject/Assets/Scripts/CutScenes/EndingController.cs
+++ theInternalProject/Assets/Scripts/CutScenes/EndingController.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class EndingController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("UI")]
+    public Text endingText;
+    public Image endingImage;
+    public Sprite goodSprite;
+    public Sprite partialSprite;
+    public Sprite badSprite;
+
     void Start()
     {
-        
-    }
+        var gameManager = GameManager.instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        EndingType ending = EndingType.Bad;
+        int saved = 0;
+        int total = 0;
+
+        if (gameManager != null)
+        {
+            ending = gameManager.GetEndingType();
+            saved = gameManager.GetSavedCount();
+            total = gameManager.GetTotalPatients();
+        }
+
+        switch (ending)
+        {
+            case EndingType.Good:
+                if (endingText != null)
+                    endingText.text = $"All {total} patients were saved.";
+                if (endingImage != null && goodSprite != null)
+                    endingImage.sprite = goodSprite;
+                AudioManager.instance?.PlayGoodEndingMusic();
+                break;
+
+            case EndingType.Partial:
+                if (endingText != null)
+                    endingText.text = $"{saved} out of {total} patients were saved.";
+                if (endingImage != null && partialSprite != null)
+                    endingImage.sprite = partialSprite;
+                AudioManager.instance?.PlayPartialEndingMusic();
+                break;
+
+            case EndingType.Bad:
+            default:
+                if (endingText != null)
+                    endingText.text = "None of the patients survived.";
+                if (endingImage != null && badSprite != null)
+                    endingImage.sprite = badSprite;
+                AudioManager.instance?.PlayBadEndingMusic();
+                break;
+        }
     }
 }
