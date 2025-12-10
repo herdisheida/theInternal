4032169e5ccc60cd5b84bbd5c56771c65fc45a89
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-10 11:48:31 +0000

    Update BossController.cs

--- theInternalProject/Assets/Scripts/Bosses/BossController.cs
+++ theInternalProject/Assets/Scripts/Bosses/BossController.cs
@@ -365,7 +365,7 @@ public class BossController : MonoBehaviour
     {
         while (phase2)
         {
-            if (!isUsingVines) yield return null;
+            if (!isUsingVines) yield break;
 
             float topY = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1.3f, 0)).y;
 
