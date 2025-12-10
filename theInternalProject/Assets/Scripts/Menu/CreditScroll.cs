2925717d1eedf00447c5687e837467f74e747724
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-10 21:39:58 +0000

    change credits background
    same as in menu

--- theInternalProject/Assets/Scripts/Menu/CreditScroll.cs
+++ theInternalProject/Assets/Scripts/Menu/CreditScroll.cs
@@ -5,7 +5,7 @@ public class CreditScroll : MonoBehaviour
     public float scrollSpeed = 40f; // adjust the speed of the scroll
     private RectTransform rectTransform;
 
-    public float delayScrollTime = 2f; // delay before starting the scroll
+    public float delayScrollTime = 1.5f; // delay before starting the scroll
 
     void Start()
     {
