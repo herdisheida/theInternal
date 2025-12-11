a66208396a6b80afd33411a46afcbaa9948a05a1
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-11 10:10:06 +0000

    only show weapons online cutscene at first patient

--- theInternalProject/Assets/Scripts/ObstacleGameplay/TunnelPortal.cs
+++ theInternalProject/Assets/Scripts/ObstacleGameplay/TunnelPortal.cs
@@ -5,11 +5,12 @@ public class TunnelPortal : MonoBehaviour
 {
 
     [Header("Scene Flow")]
-    public string nextSceneName = "WeaponOnline";
+    private string weaponCutScene = "WeaponOnline";
+    private string bossBattleScene;
 
     void Start()
     {
-        
+        gm = GameManager.instance;
     }
 
     void Update()
@@ -21,8 +22,25 @@ public class TunnelPortal : MonoBehaviour
     {
         if (!other.CompareTag("Player")) return;
 
-        AudioManager.instance?.FadeOutMusic(3f);
-        // load the next scene
-        SceneManager.LoadScene(nextSceneName);
+        if (gm == null)
+        {
+            Debug.LogWarning("TunnelPortal: GameManager instance not found.");
+            return;
+        }
+        if (gm.currentPatient == null)
+        {
+            Debug.LogWarning("TunnelPortal: currentPatient is null in GameManager.");
+            return;
+        }
+
+        if (gm.patientLevelsPlayed == 0)
+        {
+            AudioManager.instance?.FadeOutMusic(3f);
+            SceneManager.LoadScene(weaponCutScene);
+        } else {
+            bossBattleScene = gm.currentPatient.bossSceneName;
+            AudioManager.instance?.StopMusic();
+            SceneManager.LoadScene(bossBattleScene);
+        }
     }
 }
