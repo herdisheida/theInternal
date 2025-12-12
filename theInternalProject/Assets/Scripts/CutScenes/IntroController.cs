51d9019957cce7027fc0c3040987e84aeaa943d1
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-12 19:52:14 +0000

    add intro scene
    gives background to doctor and apocolipse

---
+++ theInternalProject/Assets/Scripts/CutScenes/IntroController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class IntroTextSequence : MonoBehaviour
+{
+    [Header("Text")]
+    public TextMeshProUGUI dialogueText;
+
+    [TextArea(3, 5)]
+    public string[] lines;
+
+    [Header("Timing")]
+    public float timeBetweenLines = 3f;
+
+    [Header("Scene")]
+    public string nextSceneName = "PatientSelection";
+
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        if (dialogueText == null || lines.Length == 0)
+        {
+            Debug.LogWarning("IntroTextSequence: Missing text or lines.");
+            return;
+        }
+
+        dialogueText.text = "";
+        StartCoroutine(PlaySequence());
+    }
+
+    IEnumerator PlaySequence()
+    {
+        while (currentIndex < lines.Length)
+        {
+            dialogueText.text = lines[currentIndex];
+            currentIndex++;
+
+            yield return new WaitForSeconds(timeBetweenLines);
+        }
+
+        // after last line
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
