d5f50fea77d62d54f9a4fe342d403a2aff9e2f6b
Brynhildurth <brynhildur.bjorg@gmail.com>
2025-12-12 12:30:37 +0000

    Revert "doesnt seem to find the analysistext"
    This reverts commit 9cff7be3654cccc203a78a8f008730629a603be0.

---
+++ theInternalProject/Assets/Scripts/PatientAnalysis/PatientDialogScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+public class PatientDialogScript : MonoBehaviour
+{
+    [Header("UI References")]
+    public Image TextBox;
+    public TextMeshProUGUI CharacterName;
+    public TextMeshProUGUI TemplateText;
+
+    [Header("Text lines for each patient Infected")]
+    [TextArea] public string[] ZombieInfectedLines;
+    [TextArea] public string[] WerewolfInfectedLines;
+    [TextArea] public string[] VampireInfectedLines;
+
+    [Header("Text lines for each patient Saved")]
+    [TextArea] public string[] ZombieSavedLines;
+    [TextArea] public string[] WerewolfSavedLines;
+    [TextArea] public string[] VampireSavedLines;
+
+    [Header("Timings")]
+    public float fadeDuration = 0.2f;
+    public float holdDuration = 3f;
+    public bool loopLines = false; 
+    private PatientData analizedPatient;
+
+    void Start() {
+        if (GameManager.instance == null)
+        {
+            Debug.LogError("No GameManager in scene!");
+            return;
+        }
+        analizedPatient = GameManager.instance.currentPatient;
+
+        if (analizedPatient == null)
+        {
+            Debug.LogError("GameManager.currentPatient is null");
+            return;
+        }
+    }
+
+}
