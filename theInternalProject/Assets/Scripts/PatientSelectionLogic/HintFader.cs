e2b781d07e81c562e56639beebf5f38adea63b95
Kornelius-ux <hlynurh24@ru.is>
2025-12-13 17:13:12 +0000

    changed the hint text to be more clean

--- theInternalProject/Assets/Scripts/PatientSelectionLogic/HintFader.cs
+++ theInternalProject/Assets/Scripts/PatientSelectionLogic/HintFader.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 public class HintFader : MonoBehaviour
 {
     public CanvasGroup hintGroup;
-    public float delayBeforeFade = 3f;
-    public float fadeDuration = 1f;
+    public float delayBeforeFade = 0f;
+    public float idleTime = 1f;
+    public float fadeDuration = 0f;
 
     void Start()
     {
         StartCoroutine(FadeInDelayed());
+    }
+
+    void Update()
+    {
+        if (idleTime == 0 || hintGroup.alpha ==  0){
+            StopCoroutine(FadeInDelayed());
+        }
+        else
+        {
+            idleTime += Time.deltaTime;
+        }
     }
 
     IEnumerator FadeInDelayed()
