40718c32832c505bb3e1e330d9fab3538623bd4d
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-12-14 12:41:38 +0000

    works dying

--- theInternalProject/Assets/Scripts/Managers/GameManager.cs
+++ theInternalProject/Assets/Scripts/Managers/GameManager.cs
@@ -93,7 +93,6 @@ public class GameManager : MonoBehaviour
     public void MarkPatientDead()
     {
         if (currentPatient == null) return;
-        if (currentPatient.status != PatientStatus.None) return;
 
         // Can't kill someone already saved ( doctor is not a psycho )
         if (currentPatient.status == PatientStatus.Saved)
